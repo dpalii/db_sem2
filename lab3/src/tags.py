1f0b67d91718157e7bd3b5e20d806828cbe387fb
Dmitry Palii <dpalii.study@gmail.com>
2021-05-29 22:38:46 +0300

    lab3

---
+++ lab3/src/tags.py
@@ -0,0 +1,13 @@
+from enum import Enum
+
+class Tags(Enum):
+    work = 1,
+    family = 2
+
+    @classmethod
+    def has_member(cls, value):
+        return value in Tags._member_names_
+
+    @classmethod
+    def get_members_list(cls):
+        return ', '.join(Tags._member_names_)
